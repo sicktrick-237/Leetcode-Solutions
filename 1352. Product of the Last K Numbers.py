aa022317f37e0af8c97ddd4b9400385b2343191a
SickTrick <35392350+sicktrick-237@users.noreply.github.com>
2022-07-31 15:42:02 +0530

    Updated 1352 solution

--- 1352. Product of the Last K Numbers.py
+++ 1352. Product of the Last K Numbers.py
@@ -1,40 +1,29 @@
-# from functools import reduce
-#
-# class ProductOfNumbers(object):
-#
-#     def __init__(self):
-#         self.numList = []
-#         self.productList = []
-#
-#     def add(self, num):
-#         self.numList.append(num)
-#         if len(self.numList) > 1:
-#             self.productList.append(self.productList[-1] * num)
-#         else:
-#             self.productList.append(1)
-#         print(self.productList)
-#
-#
-#
-#     def getProduct(self, k):
-#         pass
-#         # product = reduce(lambda x,y: x*y, self.numList[-k:])
-#         # print(product)
-#         # return product
-#
-# nums = [3,1,2,5,4]
-# prodls = [3, 3, 6, 30, ]
-# k = 2
-#
+import math
+
+class ProductOfNumbers(object):
+
+    def __init__(self):
+        self.numList = []
+        
+
+    def add(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        self.numList.append(num)
+        
+
+    def getProduct(self, k):
+        """
+        :type k: int
+        :rtype: int
+        """
+        return math.prod(self.numList[-k:])
+        
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
 # obj = ProductOfNumbers()
-#
-# for each in nums:
-#     obj.add(each)
-#
-# obj.getProduct(k)
-
-prd_tbl = [1, 3, 3, 6, 24]
-
-print(prd_tbl[-1]//prd_tbl[-(2+1)])
-
-print(24//3)
+# obj.add(num)
+# param_2 = obj.getProduct(k)
